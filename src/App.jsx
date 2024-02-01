0f1a8c75f20681f955753e00b5f1fcd5ab275c85
Roberth Rios <tauroroberth00@gmail.com>
2024-01-31 20:50:52 -0500

    Mi primera App con React

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+
+// Cuando importamos algo de JS o JSX no es necesario agregarlo la extension
+import Mensaje from "./components/Mensaje";
+
+// El nombre de la función que representa al componente tiene que estar en mayusculas
+function App() {
+
+  const titulo = 'Mi primera App';
+
+  // Esta funcion / componente tiene que retornar solo un elemento JSX
+  return (
+    <>
+      <h1>{titulo}</h1>
+      <section>
+        <h2 className="red">Holaaa</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident dignissimos, beatae aliquid cupiditate nemo blanditiis iusto modi aut reprehenderit nam impedit debitis voluptas eum repudiandae ullam reiciendis ad quaerat rerum. Resultado: {2 + 2}
+        </p>
+        <div>
+          <label htmlFor="idNombre">Nombre: </label>
+          <input type="text" id="idNombre" />
+        </div>
+      </section>
+      {/* Toda etiqueta debe estar cerrada con su etiqueta correspondiente */}
+      <hr />
+      <Mensaje></Mensaje>
+      <Mensaje />
+    </>
+  )
+
+}
+
+// Exportar la función
+export default App;
---
+++ src/components/Mensaje.jsx
@@ -0,0 +1,15 @@
+// Ya no es necesario importar React en componentes
+// Solo cuando se utiliza React Native
+
+const Mensaje = () => {
+    return (
+        // Las llaves vacias se conocen como Fragment, funciona como contenedor que no se renderiza
+        <>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur accusantium iure, voluptatum facilis eum dolorem obcaecati earum et, nam excepturi debitis est eligendi. Cumque molestiae delectus sapiente placeat nemo nostrum?
+            </p>
+        </>
+    )
+}
+
+export default Mensaje;
